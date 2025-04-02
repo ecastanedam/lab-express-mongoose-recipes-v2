4415503d93849055bc614d069643822b9b3dff90
Enrique Castaneda <je.castanedam@gmail.com>
2025-04-02 15:35:15 +0200

    Completed lab

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 
+
 const app = express();
 
 // MIDDLEWARE
@@ -11,34 +12,109 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const mongoose = require("mongoose");
 
+const MONGODB_URI = "mongodb://localhost:27017/express-mongoose-recipes-dev";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 
 // ROUTES
 //  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
-});
+// app.get('/', (req, res) => {
+//     res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+// });
 
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
+const Recipe = require('./models/Recipe.model');
+
+app.post('/recipes', (req, res) => {
+    Recipe.create(req.body)  
+        .then((createdRecipe) => {
+            console.log('Created recipe:', createdRecipe);
+            res.status(201).json(createdRecipe);
+        })
+        .catch((error) => {
+            console.error('Error creating recipe:', error);
+            res.status(500).json({ message: 'Error creating recipe' });
+        });
+    })
+            
 
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
 
+app.get('/recipes', (req,res) => {
+    Recipe.find()
+        .then((allRecipes) => {
+            console.log('All recipes:', allRecipes);
+            res.status(200).json(allRecipes);
+        })
+        .catch((error) => {
+            console.error('Error getting recipes:', error);
+            res.status(500).json({ error: 'Error getting recipes' });
+        })
+})
+
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+
+app.get('/recipes/:id', (req, res) => {
+    const { id } = req.params;
+    Recipe.findById(id)
+        .then((recipe) => {
+            console.log('Recipe found:', recipe);            
+            res.status(200).json(recipe);
+        })
+        .catch((error) => {
+            console.error('Error getting recipe:', error);
+            res.status(500).json({ error: 'Error getting recipe' });
+        });
+    
+})
 
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
 
+app.put('/recipes/:id', (req, res) => {
+    const { id } = req.params;  
+    Recipe.findByIdAndUpdate(id, req.body, { new: true })
+        .then((updatedRecipe) => {
+            console.log('Updated recipe:', updatedRecipe);
+            res.status(200).json(updatedRecipe);
+        })
+        .catch((error) => {
+            console.error('Error updating recipe:', error);
+            res.status(500).json({ error: 'Error updating recipe' });
+        });
+    
+})
+
+
+
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
+
+app.delete('/recipes/:id', (req, res) => {
+   Recipe.findByIdAndDelete(req.params.id) 
+        .then(() => {
+            console.log('Recipe deleted');
+            res.status(204).send();
+        })
+        .catch((error) => {
+            console.error('Error deleting recipe:', error);
+            res.status(500).json({ error: 'Error deleting recipe' });
+        });
+})
 
 
 
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,23 @@
 // Your code here ...
+//Iteration 2 - Create the Recipe model
+const mongoose = require('mongoose');
+const { Schema } = mongoose;    //const Schema = mongoose.Schema;
+
+//Create Schema
+const recipeSchema = new Schema({
+    title:{type: String, required: true, unique: true},
+    instructions: {type: String, required: true},
+    level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+    ingredients: {type: [String]},
+    image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+    duration: {type: Number, min: 0},
+    isArchived: {type: Boolean, default: false},
+    created: {type: Date, default: Date.now}
+});
+
+//Create Model
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+//Export Model
+module.exports = Recipe;
+
